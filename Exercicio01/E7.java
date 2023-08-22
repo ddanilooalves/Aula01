f8715d91526ee8655166161e3dc2f8bd7b1c56df
Danilo Alves de Aguiar <daniloalves0512@gmail.com>
2023-08-22 19:14:56 -0300

    exercicio07 implement (finsh)

--- Exercicio01/E7.java
+++ Exercicio01/E7.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
 public class E7 {
-    
+
+	public static void main(String[] args) {
+
+        try (Scanner entrada = new Scanner(System.in)) {
+            
+            System.out.print("\nInforme o valor da P.A: ");
+            int a1 = entrada.nextInt();
+            System.out.print("\nInforme o valor da razão: ");
+            int r = entrada.nextInt();
+            System.out.print("\nInforme o valor do último termo: ");
+            int n = entrada.nextInt();
+
+            int an = a1 + (n-1) * r;
+            
+            System.out.println("\nO valor da P.A é: " + an);
+        }
+    }    
 }
