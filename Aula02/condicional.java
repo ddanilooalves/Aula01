e4fc7bced9a51cb20a7330968d8b70e59eab2a5f
Danilo Alves de Aguiar <daniloalves0512@gmail.com>
2023-08-29 20:16:32 -0300

    implement operador de comparação

--- Aula02/condicional.java
+++ Aula02/condicional.java
@@ -7,6 +7,7 @@ public class condicional {
         try (Scanner entrada = new Scanner(System.in)) {
             double n1, n2, media;
             String name;
+            double frequencia;
 
             System.out.print("Informe o nome do aluno: ");
             name = entrada.nextLine();
@@ -17,16 +18,19 @@ public class condicional {
             System.out.print(name + ", informe a N2: ");
             n2 = entrada.nextDouble();
 
+            System.out.print("Informe a sua frequência em(%): ");
+            frequencia = entrada.nextDouble();
+
             media = (n1 + n2) / 2;
 
             System.out.println("Ficha do Aluno");
             System.out.println("Nome: " + name);
             System.out.println("N1: " + n1);
-        
             System.out.println("N2: " + n2);
+            System.out.println("Frequência: " + frequencia);
             System.out.println("Média: " + media);
 
-            if (media >= 6.0) {
+            if (media >= 6.0 && frequencia >= 75) {
             System.out.print("Aluno(a) APROVADO(A)!");
             } else {
             System.out.print("Aluno(a) REPROVADO(A)!");
