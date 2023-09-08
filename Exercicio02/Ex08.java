a56918a90f63aa329f1dee9f4d1b1e0b965ebb6c
Danilo Alves de Aguiar <daniloalves0512@gmail.com>
2023-09-08 18:42:45 -0300

    implement ex08 (List02)

---
+++ Exercicio02/Ex08.java
@@ -0,0 +1,33 @@
+/*08 - Faça um programa que leia um código, x e y e calcule f(x,y) de acordo com as equações
+abaixo: */
+
+package Exercicio02;
+
+import java.util.Scanner;
+
+public class Ex08 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int cod;
+        double x, y, calculo1, calculo2, calculo3, calculo4;
+
+        System.out.println("Informe o código da equação à ser calculada: ");
+        cod = entrada.nextInt();
+        System.out.println("Informe o valor de X: ");
+        x = entrada.nextDouble();
+        System.out.println("Informe o valor de Y: ");
+        y = entrada.nextDouble();
+
+        calculo1 = Math.sqrt(Math.pow(x, 3) + x * ((2 * y) - x)/ Math.pow(y, 2));
+        calculo2 = Math.sqrt((Math.pow(x, 3) + Math.pow(y, 3)) + (Math.pow(x, 3) - Math.pow(y, 3))) / x * y;
+        calculo3 = (2 * x) + Math.sqrt(x * y);
+        calculo4 = 
+
+        if (cod == 1) {
+            System.out.println("F(x,y) = " );
+        }
+
+    }
+    
+}
