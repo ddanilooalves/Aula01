3121f5cf13de8a1b4d6bdb464e549496a8a354b4
Danilo Alves de Aguiar <daniloalves0512@gmail.com>
2023-09-12 19:02:50 -0300

    implement exe08 (List02)

--- Exercicio02/Ex08.java
+++ Exercicio02/Ex08.java
@@ -8,26 +8,34 @@ import java.util.Scanner;
 public class Ex08 {
 
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        int cod;
-        double x, y, calculo1, calculo2, calculo3, calculo4;
+        try (Scanner entrada = new Scanner(System.in)) {
+            int cod;
+            double x, y, calculo1, calculo2, calculo3, calculo4;
 
-        System.out.println("Informe o código da equação à ser calculada: ");
-        cod = entrada.nextInt();
-        System.out.println("Informe o valor de X: ");
-        x = entrada.nextDouble();
-        System.out.println("Informe o valor de Y: ");
-        y = entrada.nextDouble();
+            System.out.println("Informe o código da equação à ser calculada: ");
+            cod = entrada.nextInt();
+            System.out.println("Informe o valor de X: ");
+            x = entrada.nextDouble();
+            System.out.println("Informe o valor de Y: ");
+            y = entrada.nextDouble();
 
-        calculo1 = Math.sqrt(Math.pow(x, 3) + x * ((2 * y) - x)/ Math.pow(y, 2));
-        calculo2 = Math.sqrt((Math.pow(x, 3) + Math.pow(y, 3)) + (Math.pow(x, 3) - Math.pow(y, 3))) / x * y;
-        calculo3 = (2 * x) + Math.sqrt(x * y);
-        calculo4 = 
+            calculo1 = Math.sqrt(Math.pow(x, 3) + x * ((2 * y) - x)/ Math.pow(y, 2));
+            calculo2 = Math.sqrt((Math.pow(x, 3) + Math.pow(y, 3)) + (Math.pow(x, 3) - Math.pow(y, 3))) / x * y;
+            calculo3 = (2 * x) + Math.sqrt(x * y);
+            calculo4 = Math.sin(x) + Math.cos(y);
 
-        if (cod == 1) {
-            System.out.println("F(x,y) = " );
+            if (cod == 1) {
+                System.out.println("F(x,y) = " + calculo1);
+            }
+            if (cod == 2) {
+                System.out.println("F(x,y) = " + calculo2);
+            }
+            if (cod == 3) {
+                System.out.println("F(x,y) = " + calculo3);
+            }
+            if (cod == 4) {
+                System.out.println("F(x,y) = " + calculo4);
+            }
         }
-
     }
-    
 }
