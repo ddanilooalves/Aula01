7516fba3710c873c74cf265d4de062787f0a44af
Danilo Alves de Aguiar <daniloalves0512@gmail.com>
2023-09-16 14:59:39 -0300

    implement exe18 (List02)

---
+++ Exercicio02/Ex18.java
@@ -0,0 +1,27 @@
+/*Desenhe a seguinte pirâmide de asteriscos. O usuário deve determinar a quantidade de
+linhas. */
+
+package Exercicio02;
+
+import java.util.Scanner;
+
+public class Ex18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int linha = 1, qntLinhas, i;
+        String saida = "";
+
+        System.out.println("Informe a quantidade de linhas desejadas para construção da pirâmide de asteriscos: ");
+        qntLinhas = sc.nextInt();
+
+        while (linha <= qntLinhas) {
+            i = 1;
+            while (i <= linha) {
+                System.out.print("*");
+                i = i + 1;
+            }
+            System.out.print("\n");
+            linha = linha + 1;
+        }
+    }
+}
