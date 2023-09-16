008ebdfe9df385951dcc23456382cff652f5aed3
Danilo Alves de Aguiar <daniloalves0512@gmail.com>
2023-09-16 11:58:17 -0300

    correction exe16 (List02)

--- Exercicio02/Ex16.java
+++ Exercicio02/Ex16.java
@@ -8,24 +8,25 @@ import java.util.Scanner;
 
 public class Ex16 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N, cont = 0;;
+        try (Scanner sc = new Scanner(System.in)) {
+            int N, cont = 0;;
 
-        System.out.println("Informe um número N: ");
-        N = sc.nextInt();
+            System.out.println("Informe um número N: ");
+            N = sc.nextInt();
 
-        System.out.println("Os números primos entre " + 1 + " e " + N + " são: ");
+            System.out.println("Os números primos entre " + 1 + " e " + N + " são: ");
 
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= i; j++) {
-                if (i % j == 0) {
-                    cont++;
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= i; j++) {
+                    if (i % j == 0) {
+                        cont++;
+                    }
                 }
+                if (cont == 2) {
+                    System.out.println(i);
+                }
+                cont = 0;
             }
-            if (cont == 2) {
-                System.out.println(i);
-            }
-            cont = 0;
         }
     }
     
