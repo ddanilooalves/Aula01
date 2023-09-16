3cf317bf8171bd3bf69ec88dc17e1b39b557db27
Danilo Alves de Aguiar <daniloalves0512@gmail.com>
2023-09-16 17:57:30 -0300

    correction exe18 (List02)

--- Exercicio02/Ex18.java
+++ Exercicio02/Ex18.java
@@ -7,21 +7,21 @@ import java.util.Scanner;
 
 public class Ex18 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int linha = 1, qntLinhas, i;
-        String saida = "";
+        try (Scanner sc = new Scanner(System.in)) {
+            int linha = 1, qntLinhas, i;
 
-        System.out.println("Informe a quantidade de linhas desejadas para construção da pirâmide de asteriscos: ");
-        qntLinhas = sc.nextInt();
+            System.out.println("Informe a quantidade de linhas desejadas para construção da pirâmide de asteriscos: ");
+            qntLinhas = sc.nextInt();
 
-        while (linha <= qntLinhas) {
-            i = 1;
-            while (i <= linha) {
-                System.out.print("*");
-                i = i + 1;
+            while (linha <= qntLinhas) {
+                i = 1;
+                while (i <= linha) {
+                    System.out.print("*");
+                    i = i + 1;
+                }
+                System.out.print("\n");
+                linha = linha + 1;
             }
-            System.out.print("\n");
-            linha = linha + 1;
         }
     }
 }
