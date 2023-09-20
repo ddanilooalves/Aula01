7fb94bd5056104d963ab111fd16a32ebc630e5e7
Danilo Alves de Aguiar <daniloalves0512@gmail.com>
2023-09-19 21:14:46 -0300

    implement exe19 (List02)

--- Exercicio02/Ex19.java
+++ Exercicio02/Ex19.java
@@ -7,19 +7,29 @@ import java.util.Scanner;
 public class Ex19 {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
-            int linha = 1, qntLinhas, i;
+            int qntLinhas,linha = 0;
 
             System.out.println("Informe a quantidade de linhas desejadas para construção da pirâmide de asteriscos: ");
             qntLinhas = sc.nextInt();
 
-            while (linha <= qntLinhas) {
-                i = 1;
-                while (i <= linha) {
+            double divisor = qntLinhas / 2;
+
+            System.out.println("Quantidade de linhas: " + qntLinhas);
+
+            for(int i = 0; i <= qntLinhas ; i++) {
+                String out = "";
+                if (i < divisor) {
+                    for (int j = 0; j <= linha; j++) {
                     System.out.print("*");
-                    i = i + 1;
+                    }
+                    linha++;
+                } else {
+                    for (int k = 0; k <= linha; k++) {
+                        System.out.print("*");
+                    }
+                    linha--;
                 }
-                System.out.print("\n");
-                linha = linha + 1;
+                System.out.println(out);
             }
         }
     }
