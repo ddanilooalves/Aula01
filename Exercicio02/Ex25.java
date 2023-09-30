0e2153696efd3d761dbf5e2111a753e99016ac24
Danilo Alves de Aguiar <daniloalves0512@gmail.com>
2023-09-30 16:18:34 -0300

    correction exe25 (List02)

--- Exercicio02/Ex25.java
+++ Exercicio02/Ex25.java
@@ -9,67 +9,67 @@ import java.util.Scanner;
 
 public class Ex25 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Digite o tamanho dos vetores: ");
+            int N = sc.nextInt();
 
-        System.out.println("Digite o tamanho dos vetores: ");
-        int N = sc.nextInt();
-
-        if (N <= 0 || N > 10) {
-            System.out.println("N deve estar entre 1 e 10!");
-            return;
-        }
-
-        double V1[] = new double[N];
-        double V2[] = new double[N];
-        char[] Op = new char[N];
-        double[] Resul = new double[N];
-
-        System.out.println("Digite os valores do Vetor 1: ");
-        for (int i = 0; i < N; i++) {
-            V1[i] = sc.nextDouble();
-        }
-
-        System.out.println("Digite os valores do Vetor 2: ");
-        for (int i = 0; i < N; i++) {
-            V2[i] = sc.nextDouble();
-        }
-
-        System.out.println("Digite os códigos operacionais: ");
-        for (int i = 0; i < N; i++) {
-            Op[i] = sc.next().charAt(0);
-        }
-
-        for (int i = 0; i < N; i++) {
-            switch (Op[i]) {
-                case '+':
-                    Resul[i] = V1[i] + V2[i];
-                    break;
-                
-                case '-':
-                    Resul[i] = V1[i] - V2[i];
-                    break;
-
-                case '*':
-                    Resul[i] = V1[i] * V2[i];
-                    break;
-
-                case '/':
-                    if (V2[i] != 0) {
-                        Resul[i] = V1[i] / V2[i];
-                    } else {
-                        System.out.println("Divisão por zero.");
-                        return;
-                    }
-                    break;
-                default:
-                    System.out.println("Operação inválida: " + Op[i]);
-                    return;
+            if (N <= 0 || N > 10) {
+                System.out.println("N deve estar entre 1 e 10!");
+                return;
             }
-        }
 
-        System.out.println("### Resultado das operações ##");
-        for (int i = 0; i < N; i++) {
-            System.out.println("Resultado [" + i + "] - " + Resul[i]);
+            double V1[] = new double[N];
+            double V2[] = new double[N];
+            char[] Op = new char[N];
+            double[] Resul = new double[N];
+
+            System.out.println("Digite os valores do Vetor 1: ");
+            for (int i = 0; i < N; i++) {
+                V1[i] = sc.nextDouble();
+            }
+
+            System.out.println("Digite os valores do Vetor 2: ");
+            for (int i = 0; i < N; i++) {
+                V2[i] = sc.nextDouble();
+            }
+
+            System.out.println("Digite os códigos operacionais: ");
+            for (int i = 0; i < N; i++) {
+                Op[i] = sc.next().charAt(0);
+            }
+
+            for (int i = 0; i < N; i++) {
+                switch (Op[i]) {
+                    case '+':
+                        Resul[i] = V1[i] + V2[i];
+                        break;
+                    
+                    case '-':
+                        Resul[i] = V1[i] - V2[i];
+                        break;
+
+                    case '*':
+                        Resul[i] = V1[i] * V2[i];
+                        break;
+
+                    case '/':
+                        if (V2[i] != 0) {
+                            Resul[i] = V1[i] / V2[i];
+                        } else {
+                            System.out.println("Divisão por zero.");
+                            return;
+                        }
+                        break;
+                    default:
+                        System.out.println("Operação inválida: " + Op[i]);
+                        return;
+                }
+            }
+
+            System.out.println("### Resultado das operações ##");
+            for (int i = 0; i < N; i++) {
+                System.out.println("Resultado [" + i + "] - " + Resul[i]);
+            }
         }
     }
     
